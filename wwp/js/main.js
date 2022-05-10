bc7d6b546e58e2af59fa8ed4446347ad210f3d92
gouws-j <1948061@students.wits.ac.za>
2022-05-10 10:23:37 +0200

    Added comments for review

--- wwp/js/main.js
+++ wwp/js/main.js
@@ -1,10 +1,15 @@
+// Initial rendition of the game logic.
+// Event listener for inputs.
 document.addEventListener('DOMContentLoaded', () => {
+  // setup of squares.
   createSquares()
 
+  // keyboard keys init.
   const keys = document.querySelectorAll('.keyboard-row button')
   const guessedWords = [[]]
   let availableSpace = 1
 
+  // keys will update on click - for now Enter and Del need to be added in.
   for (let i = 0; i < keys.length; i++) {
     keys[i].onclick = ({ target }) => {
       const letter = target.getAttribute('data-key')
@@ -12,12 +17,13 @@ document.addEventListener('DOMContentLoaded', () => {
       updateGuessedWords(letter)
     }
   }
-
+  // Array for guessed words. Currently a placeholder.
   function getCurrentWordArr () {
     const numberOfGuessedWords = guessedWords.length
     return guessedWords[numberOfGuessedWords - 1]
   }
 
+  // The function for tracking and updating guessed words. Shows the input letter on screen when clicked, but doesn't register a word yet.
   function updateGuessedWords (letter) {
     const currentWordArr = getCurrentWordArr()
 
@@ -30,7 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
       availableSpaceEl.textContent = letter
     }
   }
-
+  // Function for creating squares.
   function createSquares () {
     const gameBoard = document.getElementById('board')
 
