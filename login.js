0e3f455e64322ae6841cba624de44303025b4eba
Robin Jonker <robinjonker742@gmail.com>
2022-05-17 13:07:38 +0200

    unit and web-driver testing and additional js function for possible page redirection

--- login.js
+++ login.js
@@ -7,3 +7,13 @@ function validateJS () {
     alert('Incorrect username/password!')
   }
 }
+
+exports.toHome = (username, password) => {
+  let msg
+  if (username === 'admin' && password === 'admin') {
+    msg = 'Go to Home' // this would be a link to the home page
+  } else {
+    msg = 'Do not go to Home'
+  }
+  return msg
+}
