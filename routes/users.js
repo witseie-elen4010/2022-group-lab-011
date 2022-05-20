061f480d0b79bda482a54c76e6e85236256bb210
Robin Jonker <robinjonker742@gmail.com>
2022-05-20 15:55:45 +0200

    express framework, login page recreated

---
+++ routes/users.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    console.log(req.query.username)
+    res.send('User List')
+})
+
+router.get('/new', (req, res) => {
+    res.render("users/new")
+})
+
+router.post('/', (req, res) => {
+    const isValid = true
+    let x = req.body.username
+    let y = req.body.password
+    if (isValid) {
+        users.push( {username: x, password: y})
+        z = users.length - 1
+        res.redirect('/users/'+z)
+    } else {
+        console.log("Error")
+        res.render('users/new', { username: x})
+    }
+    console.log(x)
+    res.send("Hi "+x)
+})
+
+router
+    .route("/:id")
+    .get((req, res) => {
+        console.log(req.user)
+        let x = req.params.id
+        res.send('Get User with ID '+x)
+    })
+    .put((req, res) => {
+        let x = req.params.id
+        res.send('Update User with ID '+x)
+    })
+    .delete((req, res) => {
+        let x = req.params.id
+        res.send('Delete User with ID '+x)
+    })
+
+const users = [{ username: "Kyle", password: "1234"}, { username: "Sally", password: "1234"}]
+
+router.param('id', (req, res, next, id) => {
+    req.user = users[id]
+    next()
+})
+
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.set('view engine', 'ejs')
+app.use(logger)
+
+const userRouter = require('./routes/users')
+
+app.use('/users', userRouter)
+
+function logger(req, res, next) {
+    console.log(req.originalUrl)
+    next()
+}
+
+app.listen(3000)
