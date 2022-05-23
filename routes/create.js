4f6b9058362792f7eafabfd21997d69f8275e37d
Robin Jonker <robinjonker742@gmail.com>
2022-05-23 11:36:57 +0200

    create registration page

---
+++ routes/create.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.render('users/create')
+})
+
+router.post('/', (req, res) => {
+    let a = req.body.username
+    let b = req.body.email
+    let c = req.body.password
+    let d = req.body.confirm_password
+    console.log(a)
+    console.log(b)
+    console.log(c)
+    console.log(d)
+    res.redirect('/users/new')
+})
+
+module.exports = router
