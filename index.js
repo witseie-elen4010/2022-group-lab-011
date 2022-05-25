15dff3f961f2dcfa0c22455001680e8de6bca7b9
Robin Jonker <robinjonker742@gmail.com>
2022-05-25 18:17:21 +0200

    refactored the files so names align and make sense

--- index.js
+++ index.js
@@ -11,17 +11,17 @@ app.use(express.json())
 app.set('view engine', 'ejs')
 app.use(logger)
 
-const createRouter = require('./routes/create')
-const userRouter = require('./routes/users')
+const createRouter = require('./routes/create_account')
+const loginRouter = require('./routes/login')
 const homeRouter = require('./routes/home')
-const gameRouter = require('./routes/game')
+const gameRouter = require('./routes/solo_game')
 const leadRouter = require('./routes/leaderboard')
 
 //Define routes
-app.use('/create', createRouter)
-app.use('/users', userRouter)
+app.use('/create_account', createRouter)
+app.use('/login', loginRouter)
 app.use('/home', homeRouter)
-app.use('/game', gameRouter)
+app.use('/solo_game', gameRouter)
 app.use('/leaderboard', leadRouter)
 
 
--- routes/solo_game.js
+++ routes/solo_game.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.render("users/game")
+    res.render("users/solo_game")
 })
 
 module.exports = router
