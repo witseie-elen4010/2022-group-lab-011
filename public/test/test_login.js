fbec873bedc99f3e604ac712bd7682d0ffff5422
Ferreira-VN <1834038@students.wits.ac.za>
2022-05-30 16:06:20 +0200

    Create test_login.js

---
+++ public/test/test_login.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const {Builder, By, Key, util} = require("selenium-webdriver");
+const assert = require("assert");
+var should = require("chai").should();
+
+describe("Testing login.ejs", function(){
+    it("Login page opened", async function(){
+
+         let driver = await new Builder().forBrowser("firefox").build();
+         await driver.get("https://wordleworldparty.azurewebsites.net/");
+
+         await driver.findElement(By.xpath("//a")).click();
+         let heading = await driver.findElement(By.xpath("//h1")).getText().then(function(value){
+              return value;
+         });
+
+         heading.should.equal("Login Page")
+
+         await driver.quit();
+     });
+    });
