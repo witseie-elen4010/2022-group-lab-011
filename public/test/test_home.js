f952c15c15a38ad900a16f3eee2d689f2ba074a0
Ferreira-VN <1834038@students.wits.ac.za>
2022-05-30 17:34:39 +0200

    Test button directs to solo word game

--- public/test/test_home.js
+++ public/test/test_home.js
@@ -26,6 +26,27 @@ describe('Test Home page', function () {
 
     await driver.quit()
     })
+    it('Test button redirects to solo game', async function () {
+        const driver = await new Builder().forBrowser('firefox').build()
     
+            await driver.get('https://wordleworldparty.azurewebsites.net/')
+            // The above will only work while no login is required to get to homepage
+            await driver.findElement(By.xpath("//a")).click();
+            
+            driver.findElement(By.id("username")).sendKeys("123");
+            driver.findElement(By.id("password")).sendKeys("123");
+      
+            await driver.findElement(By.id("myButton")).click();
+            await driver.findElement(By.xpath("//button")).click();
+            await driver.findElement(By.id("sologame")).click();
+    
+            let heading = await driver.findElement(By.xpath('//h1')).getText().then(function (value) {
+                return value
+            })
+    
+            heading.should.equal('Wordle World Party')// fix this
+    
+        await driver.quit()
+        })
 
 })
