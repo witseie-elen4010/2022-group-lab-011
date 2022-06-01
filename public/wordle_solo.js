dc3bfdcc30fc1640b19294d3b220181db56056f8
Robin Jonker <robinjonker742@gmail.com>
2022-06-01 21:26:55 +0200

    Refractor Game Log Page and conditional prints

--- public/wordle_solo.js
+++ public/wordle_solo.js
@@ -4,34 +4,7 @@ const messageDisplay = document.querySelector('.message-container')
 
 //setup
 const keys = [
-    'Q',
-    'W',
-    'E',
-    'R',
-    'T',
-    'Y',
-    'U',
-    'I',
-    'O',
-    'P',
-    'A',
-    'S',
-    'D',
-    'F',
-    'G',
-    'H',
-    'J',
-    'K',
-    'L',
-    'ENTER',
-    'Z',
-    'X',
-    'C',
-    'V',
-    'B',
-    'N',
-    'M',
-    'Del',
+    'Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','ENTER','Z','X','C','V','B','N','M','Del',
 ]
 
 const wordEntry = [
