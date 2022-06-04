570b62463b766b3a6c572f6ae077ddb3d23240ff
Ferreira-VN <1834038@students.wits.ac.za>
2022-06-04 18:31:56 +0200

    Test button on landing page
    Using Puppeteer

--- public/_tests_/functions.test.js
+++ public/_tests_/functions.test.js
@@ -1,17 +1,20 @@
+/* eslint-env jest*/
 const puppeteer = require('puppeteer');
 
+jest.setTimeout(60000);
+// Test that button click on landing page directs to Login Page
 describe('Test index page:', () => {
     it('Button says login', async () => {
         
-        //beforeEach(async () => {
-            const browser = await puppeteer.launch();
-            const page = await browser.newPage();
-            await page.goto('https://wordleworldparty.azurewebsites.net/')
-        //})
-        
-        const btn = page.$('#btn');
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('https://wordleworldparty.azurewebsites.net/')
 
-        let btnText = await page.evaluate(el => el.textContent, btn);
-        expect(+btnText).toEqual('Login');
+        
+        const searchBtn =  await page.$x("//*[@id='button-1']");
+        await page.click('[name="landing"]')
+        await expect(page.title()).resolves.toMatch('Login Page')
+        await browser.close();
     })
+
 })
