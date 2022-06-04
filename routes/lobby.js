3adaa0f3e7bcb4be45e8bf388f15ff3260ecaa24
Robin Jonker <robinjonker742@gmail.com>
2022-06-04 23:01:28 +0200

    enable public lobby page

--- routes/lobby.js
+++ routes/lobby.js
@@ -10,24 +10,5 @@ router.get('/', (req, res) => {
   }
 })
 
-router.post('/', (req, res) => {
-    if (!req.session.ID) {
-      res.redirect('/login')
-      } else {
-    console.log('Button was clicked')
-    const playerRole = req.body.game_role
-    const adminWord = req.body.admin_input
-    if (playerRole === 1 && adminWord ) {
-      const word = req.body.admin_input
-      checkWord(word)
-      // set /word page variable - in here for now as backup
-      router.get('/word-admin', (req, res) => {
-        res.json(word)
-      })
-    } else {
-      res.redirect('/multi_game')
-    }
-  }
-}) 
   
 module.exports = router
