f90554060f9aa89a5e0e665cf38d6d97ef102505
1862166-TB <1862166@students.wits.ac.za>
2022-06-05 15:45:43 +0200

    Test of two more button redirects

--- public/_tests_/home.test.js
+++ public/_tests_/home.test.js
@@ -22,11 +22,12 @@ describe('Test home page:', () => {
         // click login
         await page.click('[name="loginbutton"]');
         // New page should be hompage
+        await page.waitForTimeout(10);
         await expect(page.title()).resolves.toMatch('Homepage');
         await browser.close();
     })
     it('Dropdown button is clickable', async () => {
-        const browser = await puppeteer.launch({headless: false})
+        const browser = await puppeteer.launch()
         const page = await browser.newPage()
         await page.setViewport({width: 1920, height: 1080});
         await page.goto('https://wordleworldparty.azurewebsites.net/')
@@ -42,6 +43,56 @@ describe('Test home page:', () => {
         await page.click('[name="loginbutton"]');
         // Now on Homepage: click Select game mode
         await page.click('[name=dropdownbutton]');
+
+        await browser.close();
+    })
+    it('Click Solo Word Game which redirects to Solo page:', async () => {
+        const browser = await puppeteer.launch()
+        const page = await browser.newPage()
+        await page.setViewport({width: 1920, height: 1080});
+        await page.goto('https://wordleworldparty.azurewebsites.net/')
+
+        await page.click('[name="landing"]');
+
+        await page.waitForSelector('input[name="username"]');
+        await page.$eval('input[name="username"]', el => el.value = '123');
+
+        await page.waitForSelector('input[name="password"]');
+        await page.$eval('input[name="password"]', el => el.value = '123');
+
+        await page.click('[name="loginbutton"]');
+        // Now on Homepage: click Select game mode
+        await page.click('[name=dropdownbutton]');
+        // Click solo word game:
+        await page.click('[name="sologame"]')
+        
+        await page.waitForTimeout(10);
+        await expect(page.title()).resolves.toMatch('Wordle');
+
+        await browser.close();
+    })
+    it('Click Multiplayer Lobby which redirects to Multi page:', async () => {
+        const browser = await puppeteer.launch()
+        const page = await browser.newPage()
+        await page.setViewport({width: 1920, height: 1080});
+        await page.goto('https://wordleworldparty.azurewebsites.net/')
+
+        await page.click('[name="landing"]');
+
+        await page.waitForSelector('input[name="username"]');
+        await page.$eval('input[name="username"]', el => el.value = '123');
+
+        await page.waitForSelector('input[name="password"]');
+        await page.$eval('input[name="password"]', el => el.value = '123');
+
+        await page.click('[name="loginbutton"]');
+        // Now on Homepage: click Select game mode
+        await page.click('[name=dropdownbutton]');
+        // Click solo word game:
+        await page.click('[name="lobby"]')
+        
+        await page.waitForTimeout(10);
+        await expect(page.title()).resolves.toMatch('Lobby');
 
         await browser.close();
     })
