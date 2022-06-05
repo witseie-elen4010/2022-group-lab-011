e9b85708006421de2662142f1794694c358ff745
Ferreira-VN <1834038@students.wits.ac.za>
2022-06-05 15:29:19 +0200

    Tests login page reloads when password is incorrect

--- public/_tests_/login.test.js
+++ public/_tests_/login.test.js
@@ -49,6 +49,26 @@ describe('Test index page:', () => {
         await expect(page.title()).resolves.toMatch('Homepage');
         await browser.close();
     })
-   
+    
+    it('Login page reloads when password is incorrect:', async () => {
+
+        const browser = await puppeteer.launch()
+        const page = await browser.newPage()
+        await page.setViewport({width: 1920, height: 1080});
+        await page.goto('https://wordleworldparty.azurewebsites.net/')
+        
+        await page.click('[name="landing"]');
+
+        await page.waitForSelector('input[name="username"]');
+        await page.$eval('input[name="username"]', el => el.value = '123');
+
+        await page.waitForSelector('input[name="password"]');
+        await page.$eval('input[name="password"]', el => el.value = '1234');
+
+        await page.click('[name="loginbutton"]');
+
+        await expect(page.title()).resolves.toMatch('Login Page');
+        await browser.close();
+    })
 })
 
