e8757e9fb5fa958c0a4b69068df867c29cde9c66
1862166-TB <1862166@students.wits.ac.za>
2022-06-05 18:23:08 +0200

    Update wordle_solo tests

--- public/_tests_/wordle_solo.test.js
+++ public/_tests_/wordle_solo.test.js
@@ -5,10 +5,10 @@ const timeout = 120000;
 jest.setTimeout(timeout);
 
 describe('Test the solo game page:', () => {
-    it('Test the logout button', async() => {
+    it('Click Solo Word Game which redirects to Solo page:', async () => {
         const browser = await puppeteer.launch()
         const page = await browser.newPage()
-        await page.setViewport({width: 3840, height: 2160});
+        await page.setViewport({width: 1920, height: 1080});
         await page.goto('https://wordleworldparty.azurewebsites.net/')
         // click Let's Get Started
         await page.click('[name="landing"]');
@@ -25,13 +25,10 @@ describe('Test the solo game page:', () => {
         await page.click('[name=dropdownbutton]');
         // Click solo word game:
         await page.click('[name="sologame"]')
-        // Click dropdown
-        await page.click('[id="dropdownMenuButton1"]')
-        await page.waitForTimeout(10);
-        await page.click('[name="logout"]')
         
-        await expect(page.title()).resolves.toMatch('Login Page'); 
+        await page.waitForTimeout(10);
+        await expect(page.title()).resolves.toMatch('Wordle'); //Wordle Solo
         await page.waitForTimeout(1000);
-        await browser.close();  
+        await browser.close();
     })
 })
