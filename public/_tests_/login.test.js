f172ace809c859b606cfe348e2aa15f595b727c1
Ferreira-VN <1834038@students.wits.ac.za>
2022-06-05 15:27:18 +0200

    test button redirects to create account page

--- public/_tests_/login.test.js
+++ public/_tests_/login.test.js
@@ -1,6 +1,6 @@
 /* eslint-env jest*/
 const puppeteer = require('puppeteer');
-const timeout = 60000;
+const timeout = 90000;
 jest.setTimeout(timeout);
 
 describe('Test index page:', () => {
@@ -16,5 +16,19 @@ describe('Test index page:', () => {
         await expect(page.title()).resolves.toMatch('Login Page');
         await browser.close();
      })
+     it("Register button reverts to Login Page:", async () => {
+
+        const browser = await puppeteer.launch()
+        
+        const page = await browser.newPage()
+        await page.setViewport({width: 1920, height: 1080});
+        await page.goto('https://wordleworldparty.azurewebsites.net/')
+
+        await page.click('[name="landing"]');
+        await page.click('[name="registerbutton"]');
+        await expect(page.title()).resolves.toMatch('Create account');
+        await browser.close();
+     })
+    
 })
 
