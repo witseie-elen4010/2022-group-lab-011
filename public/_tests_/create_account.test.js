9bd83e11802eddd42145759e920a1420f53b9c96
Ferreira-VN <1834038@students.wits.ac.za>
2022-06-05 18:11:55 +0200

    Test that existing account can't be created

--- public/_tests_/create_account.test.js
+++ public/_tests_/create_account.test.js
@@ -19,7 +19,7 @@ describe('Test Create Account Page:', () => {
         await page.waitForTimeout(1000);
         await browser.close();
      })
-     it("Test existing account can't be created because passwords don't match", async () => {
+     it("Test account can't be created because passwords don't match", async () => {
         // Open browser
         const browser = await puppeteer.launch()
         const page = await browser.newPage()
@@ -30,16 +30,46 @@ describe('Test Create Account Page:', () => {
         await page.click('[name="registerbutton"]');
         // enter username
         await page.waitForSelector('input[name="username"]');
-        await page.$eval('input[name="username"]', el => el.value = 'SeleniumTester');
+        await page.$eval('input[name="username"]', el => el.value = 'PuppeteerTester');
         // enter email
         await page.waitForSelector('input[name="email"]');
-        await page.$eval('input[name="email"]', el => el.value = 'SeleniumTester@gmail.com');
+        await page.$eval('input[name="email"]', el => el.value = 'PuppeteerTester@gmail.com');
         // enter password
         await page.waitForSelector('input[name="password"]');
         await page.$eval('input[name="password"]', el => el.value = 'Tests');
         // confirm password
         await page.waitForSelector('input[name="confirm_password"]');
         await page.$eval('input[name="confirm_password"]', el => el.value = 'Test');
+        
+        // Now click Register, should stay on same page
+        await page.waitForTimeout(10);
+        await page.click('[name="createbutton"]');
+        await expect(page.title()).resolves.toMatch('Create account');
+
+        await page.waitForTimeout(1000);
+        await browser.close();
+    })
+    it("Test existing account can't be created", async () => {
+        // Open browser
+        const browser = await puppeteer.launch()
+        const page = await browser.newPage()
+        await page.setViewport({width: 1920, height: 1080});
+        await page.goto('https://wordleworldparty.azurewebsites.net/')
+        // click Let's Get Started
+        await page.click('[name="landing"]');
+        await page.click('[name="registerbutton"]');
+        // enter username
+        await page.waitForSelector('input[name="username"]');
+        await page.$eval('input[name="username"]', el => el.value = 'PuppeteerTester');
+        // enter email
+        await page.waitForSelector('input[name="email"]');
+        await page.$eval('input[name="email"]', el => el.value = 'PuppeteerTester@gmail.com');
+        // enter password
+        await page.waitForSelector('input[name="password"]');
+        await page.$eval('input[name="password"]', el => el.value = 'Tests');
+        // confirm password
+        await page.waitForSelector('input[name="confirm_password"]');
+        await page.$eval('input[name="confirm_password"]', el => el.value = 'Tests');
         
         // Now click Register, should stay on same page
         await page.waitForTimeout(10);
